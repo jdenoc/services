4f8a9fb7f9d52e20461adddcd8e7fda80497803e
jdenoc <jdenoc@gmail.com>
2014-08-03 20:45:47 -0500

    * Added an example DB config file. * Added a .gitignore file. * Updated responses to be JSON by default. * Disabled auto init for database.php config file.

---
+++ application/config/example.db_config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: denis
+ * Date: 2014-07-26
+ */
+
+return array(
+    'hostname' => 'localhost',
+    'username' => '',           // TODO - need value
+    'password' => '',           // TODO - need value
+    'database' => '',           // TODO - need value
+    'dbdriver' => 'mysql',
+    'dbprefix' => '',
+    'pconnect' => TRUE,
+    'db_debug' => TRUE
+);
