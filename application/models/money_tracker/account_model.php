6fdc5cc25c1b6c6691196cdebc9d6795ef6bb10c
jdenoc <jdenoc@gmail.com>
2014-08-13 07:11:43 -0500

    * Corrected Class name. * Added a function get_account_id_from_entry().

--- application/models/money_tracker/account_model.php
+++ application/models/money_tracker/account_model.php
@@ -4,7 +4,7 @@
  * Date: 2014-07-20
  */
 
-class Entry_model extends CI_Model {
+class Account_model extends CI_Model {
 
     private $tbl_name = 'accounts';
 
@@ -34,18 +34,25 @@ class Entry_model extends CI_Model {
         $this->db->query("LOCK TABLE accounts WRITE");
         $this->db->flush_cache();
         // SELECT total FROM accounts WHERE id=$account_id
-        $current_total = $this->db->select('total')->from($this->tbl_name)->where(array('id'=>$account_id))->get()->row_array();
+        $account = $this->db->select('total')->from($this->tbl_name)->where(array('id'=>$account_id))->get()->row_array();
         $this->db->flush_cache();
         // UPDATE accounts SET total=($current_total+$update_value) WHERE id=$account_id
-        $this->db->where(array('id'=>$account_id))->update($this->tbl_name, array('total'=>($current_total+$update_value)));
+        $this->db->where(array('id'=>$account_id))->update($this->tbl_name, array('total'=>($account['total']+$update_value)));
         $this->db->flush_cache();
         $this->db->query("UNLOCK TABLES");
     }
 
-    public function list_accounts($user_id, $start, $limit){
+    public function list_accounts(){
         // TODO - test
         // SELECT * FROM accounts
         return $this->db->from($this->tbl_name)->get()->result_array();
+    }
+
+    public function get_account_id_from_entry($entry_id){
+        // SELECT at.account_group AS id FROM entries AS e INNER JOIN account_types AS at ON at.id=e.account_type WHERE e.id=$id
+        $this->db->select("at.account_group AS id")->from('entries AS e')->where(array('e.id'=>$entry_id))->join("account_types AS at", "at.id=e.account_type", "inner");
+        $account = $this->db->get()->row_array();
+        return $account['id'];
     }
 
     public function get($user_id, $id){
