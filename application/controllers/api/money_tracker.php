56a47a8124df664b8dee29593e794329a6079f93
jdenoc <jdenoc@gmail.com>
2014-08-13 07:10:27 -0500

    Standardised how data is returned for money_tracker API requests.

--- application/controllers/api/money_tracker.php
+++ application/controllers/api/money_tracker.php
@@ -26,7 +26,7 @@ class Money_Tracker extends REST_Controller{
     public function __construct(){
         parent::__construct();
         if(!file_exists(__DIR__.$this->_db_config_file)){
-            $this->send_response('DB config file not found', true);
+            $this->send_response('DB config file not found');
         } else {
             $this->_db_config = require_once(__DIR__.$this->_db_config_file);
         }
@@ -42,13 +42,14 @@ class Money_Tracker extends REST_Controller{
         if(!empty($attachment_id)){
             $this->Attachment->delete($id, $attachment_id);
         } else {
-            $this->Attachment->delete($id);
             $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
-            $entry_data = $this->Entry->delete($id);
             $this->load->model($this->_model_dir.'account_model', 'Account', $this->_db_config);
-            $this->Account->update_balance(-1*$entry_data['value'], $entry_data['id']);
+            $this->Attachment->delete($id);
+            $entry_value = $this->Entry->delete($id);
+            $account_id = $this->Account->get_account_id_from_entry($id);
+            $this->Account->update_balance(-1*$entry_value, $account_id);
         }
-        $this->send_response(1, 200);
+        $this->send_response(1, __FUNCTION__);
     }
 
     public function entry_get() {
@@ -58,7 +59,7 @@ class Money_Tracker extends REST_Controller{
         $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
         $entry = $this->Entry->get($id);
         if(empty($entry)){
-            $this->send_response('Entry not found', true);
+            $this->send_response('Entry not found');
         }
         if($entry['has_attachment']){
             $this->load->model($this->_model_dir.'attachment_model', 'Attachment', $this->_db_config);
@@ -73,21 +74,19 @@ class Money_Tracker extends REST_Controller{
 
         $tag_ids = json_decode($entry['tags'], true);
         $entry['tags'] = $this->Entry->get_select_tags($tag_ids);
-        $this->send_response($entry);
+        $this->send_response($entry, __FUNCTION__);
     }
 
     public function count_post() {
-        // TODO - test
         $this->validate_access();
 
-        $where_array = $this->process_where_array(json_decode(base64_decode($this->post('where')), true));
+        $where_array = $this->process_where_array(json_decode(base64_decode($this->post('where'))), true);
         $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
         $total_entries = $this->Entry->count($where_array);
-        $this->send_response($this->prepare_response($total_entries, __FUNCTION__));
+        $this->send_response($total_entries, __FUNCTION__);
     }
 
     public function list_post() {
-        // TODO - test
         $this->validate_access();
 
         $limit = $this->post('limit');
@@ -96,7 +95,7 @@ class Money_Tracker extends REST_Controller{
         $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
         $entries = $this->Entry->list_entries($where_array, $start, $limit);
 
-        $this->send_response($this->prepare_response($entries, __FUNCTION__));
+        $this->send_response($entries, __FUNCTION__);
     }
 
     public function list_accounts_get(){
@@ -105,31 +104,46 @@ class Money_Tracker extends REST_Controller{
 
         $this->load->model($this->_model_dir.'account_model', 'Account', $this->_db_config);
         $accounts = $this->Account->list_accounts();
-        $this->send_response($this->prepare_response($accounts, __FUNCTION__));
+        $this->send_response($accounts, __FUNCTION__);
     }
 
     public function save_post() {
-        // TODO - finish
+        // TODO - test
         $this->validate_access();
 
         $entry_data = json_decode(base64_decode($this->post('data')), true);
-        if(empty($secret_data)){
-            $this->send_response($this->prepare_response(0, __FUNCTION__));
+        if(empty($entry_data)){
+            $this->send_response(0, __FUNCTION__);
         }
 
-        $this->load->model($this->_model_dir.'attachment_model', 'Attachment', $this->_db_config);
-        $this->Attachment->save($entry_data);
         $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
-        $this->Entry->save($entry_data);
-        //        $this->send_response($this->prepare_response(1, __FUNCTION__));
+        $this->load->model($this->_model_dir.'attachment_model', 'Attachment', $this->_db_config);
+        $this->load->model($this->_model_dir.'account_model', 'Account', $this->_db_config);
+
+        $account_id = null;
+        if(!empty($entry_data['id']) || $entry_data['id'] != -1){
+            $existing_entry_data = $this->Entry->get($entry_data['id']);
+            if(!empty($existing_entry_data)){
+                $existing_entry_data['value'] *= ($existing_entry_data['expense'] ? -1 : 1);
+                $account_id = $this->Account->get_account_id_from_entry($existing_entry_data['id']);
+                $this->Account->update_balance(-1*$entry_data['value'], $account_id);
+            }
+        }
+
+        $entry_id = $this->Entry->save($entry_data);
+        $this->Attachment->save($entry_id, $entry_data['attachments']);
+        $entry_data['value'] *= ($entry_data['expense'] ? -1 : 1);
+        if(is_null($account_id)){
+            $account_id = $this->Account->get_account_id_from_entry($entry_id);
+        }
+        $this->Account->update_balance($entry_data['value'], $account_id);
+        $this->send_response(1, __FUNCTION__);
     }
 
     public function user_key_get(){
         // TODO - rebuild / decided if I actually need this
-//        $user_id = $this->get('id');
-//        $this->validate_access($user_id);
-//         TODO - get user decryption key
-        $this->send_response($this->prepare_response("This doesn't work yet", __FUNCTION__), true);
+        // TODO - get user decryption key
+        $this->send_response("This doesn't work yet");
     }
 
     public function tags_get(){
@@ -137,7 +151,7 @@ class Money_Tracker extends REST_Controller{
 
         $this->load->model($this->_model_dir.'entry_model', 'Entry', $this->_db_config);
         $tags = $this->Entry->get_all_tags();
-        $this->response($tags, 200);
+        $this->send_response($tags, __FUNCTION__);
     }
 
     private function validate_access(){
@@ -145,7 +159,7 @@ class Money_Tracker extends REST_Controller{
         $this->load->model($this->_model_dir.'api_key_model', 'API');
         $valid_key = $this->API->validate();
         if(!$valid_key){
-            $this->send_response("Invalid API Key:".$this->API->get_header_key(), true);
+            $this->send_response("Invalid API Key:".$this->API->get_header_key());
         }
 
 //        $this->load->model('secrets/user_model', 'User', $this->_db_config);
@@ -157,38 +171,26 @@ class Money_Tracker extends REST_Controller{
         $this->_origin= $this->API->get_key_origin();
     }
 
-    private function send_response($result, $is_error=false){
-        if($is_error){
-            $code = 400;
-            $error_msg = $result;
-            $result = '';
-        } else {
+    private function send_response($data, $function=false){
+        if($function){
             $code = 200;
             $error_msg = '';
-        }
-        $this->response(array('error'=>$error_msg, 'result'=>$result), $code);
-    }
-
-    private function prepare_response($data, $function){
-        // TODO - finish...??
-        $processed_data = null;
-        if($this->_origin == 'web'){
-            $this->load->model($this->_model_dir.'Web_model');
             switch($function){
                 case 'list_accounts_get':
-                    $processed_data = $this->Web_model->web_list_accounts($data);
-                    break;
+                case 'entry_get':
+                case 'tags_get':
                 case 'list_post':
-                    $processed_data = $this->Web_model->web_list_entries($data);
+                    $result = base64_encode(json_encode($data));
                     break;
                 default:
-                    $processed_data = $data;
+                    $result = $data;
             }
         } else {
-            // TODO - load app model
+            $code = 400;
+            $error_msg = $data;
+            $result = '';
         }
-
-        return $processed_data;
+        $this->response(array('error'=>$error_msg, 'result'=>$result), $code);
     }
 
     private function process_where_array($where_array){
