3a7149933137503ef54491bd48476e895b58cc9b
jdenoc <jdenoc@gmail.com>
2014-08-13 07:14:37 -0500

    * Corrected DB usages. * Rebuilt the save() function.

--- application/models/money_tracker/attachment_model.php
+++ application/models/money_tracker/attachment_model.php
@@ -20,7 +20,7 @@ class Attachment_model extends CI_Model {
             $where_array['id'] = $id;
         }
         // DELETE FROM attachments WHERE entry_id=$entry_id AND id=$id
-        $this->db->where($where_array)->delete('attachments');
+        $this->db->where($where_array)->delete($this->tbl_name);
         if($this->count($entry_id) < 1){
             $this->db->flush_cache();
             // UPDATE entries SET has_attachment=0 WHERE entry_id=$entry_id
@@ -28,39 +28,24 @@ class Attachment_model extends CI_Model {
         }
     }
 
-    public function save($user_id, $data) {
+    public function save($entry_id, $attachments) {
         // TODO - rebuild
-//        if(empty($data['id'])){
-//            $this->insert($user_id, $data);
-//        } else {
-//            $this->update($user_id, $data);
-//        }
-    }
-
-    private function insert($user_id, $secret_data) {
-        // TODO - rebuild
-//        $this->db->insert('secrets', array(
-//            'user_id'=>$user_id,
-//            'name'=>$secret_data['name'],
-//            'url'=>$secret_data['url'],
-//            'username'=>$secret_data['username'],
-//            'encrypted_password'=>$secret_data['encrypted_password'],
-//            'password_length'=>$secret_data['password_length'],
-//            'notes'=>$secret_data['notes'],
-//            'create_stamp'=>'NOW()'
-//        ));
-    }
-
-    private function update($user_id, $secret_data) {
-        // TODO - rebuild
-//        $data = array();
-//        foreach($secret_data as $key=>$value){
-//            if(!empty($value) && $key!='id'){
-//                $data[$key] = $value;
-//            }
-//        }
-
-//        $this->db->where(array('id'=>$secret_data['id'], 'user_id'=>$user_id))->update('secrets',$data);
+        $new_attachments = array();
+        foreach($attachments as $attachment){
+            $pos = strrpos($attachment, '.');
+            $ext = substr($attachment, $pos);
+            $new_attachments[] = array(
+                'entry_id'=>$entry_id,
+                'attachment'=>$attachment,
+                'ext'=>$ext
+            );
+        }
+        if(!empty($new_attachments)){
+            $has_attachment = 1;
+            $this->db->insert_batch($this->tbl_name, $new_attachments);
+            $this->db->flush_cache();
+            $this->db->where(array('id'=>$entry_id))->update('entries', array('has_attachment'=>$has_attachment));
+        }
     }
 
     public function get_entry($entry_id){
@@ -73,7 +58,7 @@ class Attachment_model extends CI_Model {
     public function count($entry_id){
         // TODO - test
         // SELECT COUNT(*) FROM attachments WHERE entry_id=$entry_id
-        return $this->db->from($this->tbl_name)->where(array('entry_id'=>$entry_id))->get()->count_all_results();
+        return $this->db->from($this->tbl_name)->where(array('entry_id'=>$entry_id))->count_all_results();
     }
 
     public function get($user_id, $id){
