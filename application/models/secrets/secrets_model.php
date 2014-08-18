5532a28783ff3a9ba76c4d4ffe379e9b4dff1902
jdenoc <jdenoc@gmail.com>
2014-08-18 18:06:59 -0500

    Added private $_tbl_name; to hold the primarily used table name for this model.

--- application/models/secrets/secrets_model.php
+++ application/models/secrets/secrets_model.php
@@ -6,13 +6,15 @@
 
 class Secrets_model extends CI_Model {
 
+    private $_tbl_name = 'secrets';
+
     public function __construct() {
         // Call the Model constructor
         parent::__construct();
     }
 
     public function delete($user_id, $id) {
-        $this->db->where(array('user_id'=>$user_id, 'id'=>$id))->delete('secrets');
+        $this->db->where(array('user_id'=>$user_id, 'id'=>$id))->delete($this->_tbl_name);
     }
 
     public function save($user_id, $data) {
@@ -24,7 +26,7 @@ class Secrets_model extends CI_Model {
     }
 
     private function insert($user_id, $secret_data) {
-        $this->db->insert('secrets', array(
+        $this->db->insert($this->_tbl_name, array(
             'user_id'=>$user_id,
             'name'=>$secret_data['name'],
             'url'=>$secret_data['url'],
@@ -45,30 +47,30 @@ class Secrets_model extends CI_Model {
         }
 
         if(!empty($data)){
-            $this->db->where(array('id'=>$secret_data['id'], 'user_id'=>$user_id))->update('secrets',$data);
+            $this->db->where(array('id'=>$secret_data['id'], 'user_id'=>$user_id))->update($this->_tbl_name,$data);
         }
     }
 
     public function get_password($user_id, $id) {
         // SELECT encrypted_password, password_length FROM secrets WHERE id=$id AND user_id=$user_id",
-        $this->db->select('encrypted_password, password_length')->from('secrets')->where(array('id'=>$id, 'user_id'=>$user_id));
+        $this->db->select('encrypted_password, password_length')->from($this->_tbl_name)->where(array('id'=>$id, 'user_id'=>$user_id));
         return $this->db->get()->row_array();
     }
 
     public function get_names($user_id, $start, $limit){
         // SELECT id, `name` FROM secrets WHERE user_id=$user_id LIMIT $start, $limit
-        $this->db->select('id, name')->from('secrets')->where('user_id', $user_id)->limit($limit, $start*$limit);
+        $this->db->select('id, name')->from($this->_tbl_name)->where('user_id', $user_id)->limit($limit, $start*$limit);
         return $this->db->get()->result_array();
     }
 
     public function count($user_id){
         // SELECT COUNT(*) FROM secrets WHERE user_id=$user_id
-        return $this->db->from('secrets')->where('user_id', $user_id)->count_all_results();
+        return $this->db->from($this->_tbl_name)->where('user_id', $user_id)->count_all_results();
     }
 
     public function get($user_id, $id){
         // SELECT * FROM secrets WHERE user_id=$user_id AND id=$id
-        $this->db->from('secrets')->where(array('user_id'=>$user_id, 'id'=>$id));
+        $this->db->from($this->_tbl_name)->where(array('user_id'=>$user_id, 'id'=>$id));
         return $this->db->get()->row_array();
     }
 
