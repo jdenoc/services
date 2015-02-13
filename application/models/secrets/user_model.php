2c0f4ea350a16d8f2aa009581651a2be0ac08012
jdenoc <jdenoc@gmail.com>
2015-02-12 18:04:55 -0600

    * Added PHPDOCS comments. * Added code to private update method. * Added a private variable ($_updateable_table_columns) to store an array of database table columns of columns are are permitted to be updated when using the private update method. * Added code to private insert method. * Added code to public save method. * Updated public get method to return one DB table row as a single array instead of a full DB table dump of the query data. * Created public confirm_email method, which when passed an email checks if it belongs to a user. If it does, then return the user ID, otherwise return 0.

--- application/models/secrets/user_model.php
+++ application/models/secrets/user_model.php
@@ -8,38 +8,96 @@ require_once(__DIR__.'/../user_interface.php');
 
 class User_model extends CI_Model implements user_interface{
 
+    private $_updateable_table_columns = array();
+    
     public function __construct() {
         // Call the Model constructor
         parent::__construct();
+        $this->_updateable_table_columns = array("email");
     }
 
+    /**
+     * @param string $email
+     * @return int
+     */
+    public function confirm_email($email){
+        // SELECT id FROM users WHERE email=$email
+        $user = $this->db->select('id')->from(Secrets::TABLE_USERS)->where('email', $email)->get()->row_array();
+        if(empty($user)) {
+            return 0;
+        } else {
+            return $user['id'];
+        }
+    }
+
+    /**
+     * @param int $user_id
+     * @return bool
+     */
     public function validate($user_id){
         $user_data = $this->get($user_id);
         return !empty($user_data);
     }
 
+    /**
+     * @param array $user_data
+     * @return int
+     */
     public function save($user_data){
-        // TODO - do something with insert/update
+        if(isset($user_data['id'])){
+            return $this->update($user_data);
+        } else {
+            return $this->insert($user_data);
+        }
     }
 
+    /**
+     * @param int $user_id
+     */
     public function delete($user_id){
 
     }
 
+    /**
+     * @param int $user_id
+     * @return array
+     */
     public function get($user_id){
         // SELECT * FROM users WHERE id=$$user_id
-        return $this->db->from(Secrets::TABLE_USERS)->where('id', $user_id)->get()->result_array();
+        $this->db->from(Secrets::TABLE_USERS)->where('id', $user_id);
+        return $this->db->get()->row_array();
     }
 
-    private function insert(){
-
+    /**
+     * @param array $user_data
+     * @return int
+     */
+    private function insert($user_data){
+        $this->db->insert(Secrets::TABLE_USERS, array(
+            'user_id'=>$user_data['id'],
+            'email'=>$user_data['email']
+        ));
+        return $this->db->insert_id();
     }
 
-    private function update(){
+    /**
+     * @param array $user_data
+     * @return int
+     */
+    private function update($user_data){
+        $data = array();
+        foreach($user_data as $key=>$value){
+            if(in_array($key, $this->_updateable_table_columns)){
+                $data[$key] = $value;
+            }
+        }
 
+        if(!empty($data)){
+            $this->db->where(array('id'=>$user_data['id']))->update(Secrets::TABLE_USERS,$data);
+            return $user_data['id'];
+        } else {
+            return 0;
+        }
     }
 
-    public function get_key($user_id){
-
-    }
 }
