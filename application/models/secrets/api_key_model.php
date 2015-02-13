33a250b87a87e205367d890a61eae5fcee8ef5d4
jdenoc <jdenoc@gmail.com>
2015-02-12 18:23:15 -0600

    * Added PHPDOCS comments. * Api_key_model no longer implementing the interface api_key_interface. * Added constants to dictate some API Key rules (Max length, Request header, Response header). * Update the public method get_service_keys() to search the database for API keys assigned to a user that haven't expired. * Created private method generate_api_key() to generate a "unique" API key. * Created public mehtod set_return_api_key_header() to set the response header that will contain the API key as it's response. * Created public method refresh_key() to check if an existing API key has expired, and if so generate and assign a new one the that user. * Created private method save_new_api_key() to save new API keys to be associated with a designated user. * Deleted the public method get_key_origin(). * Created public method get_user_key() to retrieve user specific API key(s).

--- application/models/secrets/api_key_model.php
+++ application/models/secrets/api_key_model.php
@@ -6,35 +6,122 @@
 
 require_once(__DIR__.'/../api_key_interface.php');
 
-class Api_key_model extends CI_Model implements api_key_interface{
+class Api_key_model extends CI_Model {
+
+    CONST API_KEY_MAX_LENGTH = 32;
+    CONST REQUEST_API_HEADER = 'Authorization';
+    CONST RESPONSE_API_HEADER = "Authentication-Info:";
 
     public function __construct() {
         // Call the Model constructor
         parent::__construct();
     }
 
-    public function validate(){
+    /**
+     * @param int $user_id
+     * @return bool
+     */
+    public function validate($user_id) {
         $client = $this->get_header_key();
-        $server = $this->get_service_keys();
-        return in_array($client, $server);
+        $server_keys = $this->get_service_keys($user_id);
+        $valid = false;
+        foreach($server_keys as $user_key){
+            if($client == $user_key['api_key']){
+                $valid = true;
+                break;
+            }
+        }
+        return $valid; 
     }
 
-    public function get_header_key(){
-        return $this->input->get_request_header('Authorization');
+    /**
+     * @return string
+     */
+    public function get_header_key() {
+        return $this->input->get_request_header(self::REQUEST_API_HEADER);
     }
 
-    public function get_service_keys(){
-        // TODO - get API from local config file or DB
-        return array(
-            'web'=>'test',
-            'app'=>'app_test'
-        );
+    /**
+     * @param int $user_id
+     * @return array
+     */
+    public function get_service_keys($user_id){
+        $this->db->select('api_key')->from(Secrets::TABLE_API_KEYS)->where(array("user_id"=>$user_id, 'expired'=>0));
+        return $this->db->get()->result_array();
     }
 
-    public function get_key_origin(){
-        return array_search(
-            $this->get_header_key(),
-            $this->get_service_keys()
-        );
+    /**
+     * @param int $user_id
+     * @param string $udid
+     * @return string
+     */
+    public function get_user_key($user_id, $udid=''){
+        $this->db->from(Secrets::TABLE_API_KEYS)->where(array('user_id'=>$user_id, 'udid'=>$udid));
+        $key_data = $this->db->get()->row_array();
+        if(empty($key_data)){
+            $api_key = $this->generate_api_key();
+            $this->save_new_api_key($api_key,$udid,$user_id);
+        } else {
+            $api_key = $this->refresh_key($key_data['api_key']);
+        }
+        
+        return $api_key;
     }
+
+    /**
+     * @param string $api_key
+     * @return string
+     */
+    public function refresh_key($api_key=''){
+        if(empty($api_key)){
+           return ''; 
+        }
+        
+        $this->db->from(Secrets::TABLE_API_KEYS)->where(array("api_key"=>$api_key));
+        $key_data = $this->db->get()->row_array();
+        if(empty($key_data)){
+            $api_key = "";
+        } else {
+            if ($key_data['expiration_stamp'] < date("Y-m-d H:i:s")) {
+                $api_key = $this->generate_api_key();
+                $this->db->where(array('id' => $key_data['id']))->update(Secrets::TABLE_API_KEYS, array('expired' => 1));
+                $this->save_new_api_key($api_key, $key_data['udid'], $key_data['user_id']);
+            } else {
+                $api_key = $key_data['api_key'];
+            }
+        }
+
+        return $api_key;
+    }
+
+    /**
+     * @return string
+     */
+    private function generate_api_key() {
+        return substr(sha1(rand()), 0, self::API_KEY_MAX_LENGTH);
+    }
+
+    /**
+     * @param string $new_api_key
+     * @param string $udid
+     * @param int $user_id
+     */
+    private function save_new_api_key($new_api_key, $udid, $user_id){
+        $this->db->insert(Secrets::TABLE_API_KEYS, array(
+            'api_key'=>$new_api_key,
+            'udid'=>$udid,
+            'user_id'=>$user_id,
+            'expiration_stamp'=>date("Y-m-d H:i:s", strtotime("+1 month"))
+        ));
+    }
+
+    /**
+     * @param string $api_key
+     */
+    public function set_return_api_key_header($api_key = ''){
+        if(!empty($api_key)) {
+            header(self::RESPONSE_API_HEADER.$api_key);
+        }
+    }
+    
 }
